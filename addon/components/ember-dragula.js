b995fa4a00d7a020e446ddaeeefd389832c13f3a
Westly Wright <westly.wright@gmail.com>
2015-10-08 16:00:51 -0700

    Remove access to raw config object, use getter instead

--- addon/components/ember-dragula.js
+++ addon/components/ember-dragula.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	willInsertElement:function(){
-		var options = this.config.options || {};
+		var options = this.get('config').options || {};
 		this.set('drake', window.dragula(options));
 		this.set('parent', this);
 	},
 	didInsertElement:function(){
-		this.setEventListeners();	
+		this.setEventListeners();
 	},
 	setEventListeners:function(){
-		if(this.config.eventList){
-			this.config.eventList.forEach(function(event){
+		if(this.get('config').eventList){
+			this.get('config').eventList.forEach(function(event){
 				this.drake.on(event.name, function(){
 					this.sendAction('dragulaEvent', event.name, arguments);
 				}.bind(this));
